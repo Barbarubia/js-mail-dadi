854e64eac4481f9f79db2528ccadd4e1bc85723b
Barbarubia <dnl.desimone@gmail.com>
2022-02-24 10:41:11 -0300

    MAIL javascript versione 1.0

---
+++ Mail/js/script.js
@@ -0,0 +1,38 @@
+// Creo l'array i cui elementi sono le e-mail degli studenti della classe 56 di Boolean
+// DISCLAIMER: LE EMAIL INSERITE SONO FITTIZIE!!!
+
+let mailStudentiClasse56 = ['adelindumitru@classe56.com', 'aldo@classe56.com', 'alessandro_1@classe56.com', 'alessandro_2@classe56.com', 'alessandro_3@classe56.com', 'alessio@classe56.com', 'anthony@classe56.com', 'antonio@classe56.com', 'camilla@classe56.com', 'cristian@classe56.com', 'daniele@classe56.com', 'erika@classe56.com', 'federico@classe56.com', 'giacomo@classe56.com', 'giovanni@classe56.com', 'giuseppe@classe56.com', 'khongor@classe56.com', 'kristian@classe56.com', 'laura@classe56.com', 'luca_1@classe56.com', 'luca_2@classe56.com', 'luigi@classe56.com', 'mariagiuliana@classe56.com', 'mario@classe56.com', 'marzio@classe56.com', 'massimo@classe56.com', 'matteo@classe56.com', 'mattia@classe56.com', 'michela@classe56.com', 'mirko@classe56.com', 'paolo@classe56.com', 'piergiorgio@classe56.com', 'salvatore@classe56.com', 'samuele@classe56.com', 'simone@classe56.com', 'stefano@classe56.com']
+
+
+
+// INIZIO Versione 1.0
+// Chiedo all'utente di inserire la sua e-mail tramite comando prompt.
+// Se l'email è presente, la console restituisce il messaggio "E-mail valida".
+// Altrimenti la console restituisce il messaggio "E-mail non valida".
+
+
+// All'interno del browser genero un popup per chiedere all'utente la propria e-mail
+const insertEmail = prompt('Inserisci la tua e-mail di appartenenza alla classe 56 di Boolean:')
+
+// Definisco una variabile esterna con valore FALSE (email non presente) e la userò all'interno di un ciclo for.
+let emailPresent = false
+
+// Genero un ciclo FOR con un indice di esecuzione che va da 0 (primo elemento dell'array) al numero precedente la lunghezza dell'array (che coincide con l'ultimo elemento dell'array).
+for (let i = 0; i < mailStudentiClasse56.length; i++) {
+    // Se l'email (dato NON case-sensitive) inserita dall'utente è uguale ad uno degli elementi dell'array...
+    if (mailStudentiClasse56[i].toLowerCase() == insertEmail.toLowerCase()) {
+    // La variabile esterna precedentemente creata si trasforma in VERA
+    emailPresent = true;
+    }
+}
+
+// Al termine del ciclo FOR la variabile esterna emailPresent avrà un valore TRUE (email inserita presente nell'array) o un valore FALSE (email inserita non presente)
+
+// Genero un IF che mi visualizza in console il messaggio di validità o meno dell'email inserita
+if (emailPresent == true) {
+    console.log(`L'email inserita è valida`);
+} else {
+    console.log(`L'email inserita non è valida`);
+}
+
+// FINE Versione 1.0
